458ea270d85563b9af3129a26b27e287977726d5
Arthur Laurent <43825600+arldka@users.noreply.github.com>
2023-01-20 21:19:03 +0100

    UPDT: .gitignore

---
+++ examples/helm_package/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    artifacthub = {
+      version = "0.1.0"
+      source   = "arldka.cloud/dev/artifacthub"
+    }
+  }
+}
+
+variable "repo_name" {
+  type    = string
+  default = "artifact-hub"
+}
+
+variable "name" {
+  type    = string
+  default = "artifact-hub"
+}
+
+data "artifacthub_helm_package" "default" {
+  repo_name = var.repo_name
+  name      = var.name
+}
+
+# Returns the package_id
+output "package_id" {
+  value = data.artifacthub_helm_package.default.id
+}
