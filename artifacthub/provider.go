0da0128a8151d256bf08895b27f09593867d9bb3
Arthur Laurent <43825600+arldka@users.noreply.github.com>
2023-01-24 15:47:28 +0100

    chore(doc): adds go:generate to generate provider documentation

--- artifacthub/provider.go
+++ artifacthub/provider.go
@@ -7,6 +7,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+func init() {
+	schema.DescriptionKind = schema.StringMarkdown
+}
+
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
