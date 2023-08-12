3133658d7a0c1044d066eed1f40427ba1408b216
Arldka <43825600+arldka@users.noreply.github.com>
2023-08-12 13:21:56 +0200

    chore(ci): codecov api_key and secret were not injected from secrets

--- artifacthub/resource_user_webhook_test.go
+++ artifacthub/resource_user_webhook_test.go
@@ -112,9 +112,6 @@ func TestResourceUserWebhookCreateReadDelete(t *testing.T) {
 			ctx := context.Background()
 			rr := schema.TestResourceDataRaw(t, r.Schema, tc.input)
 			m := &Config{os.Getenv("ARTIFACTHUB_API_KEY"), os.Getenv("ARTIFACTHUB_API_KEY_SECRET")}
-			fmt.Fprintf(os.Stdout, "m: %v\n", m)
-			fmt.Fprintf(os.Stdout, "rr: %v\n", rr)
-			fmt.Fprintf(os.Stdout, "tc: %v\n", tc)
 			if tc.action == "create" {
 				err := resourceUserWebhookCreate(ctx, rr, m)
 				if tc.wantErr {
